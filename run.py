fe3793e3c165b302cc6c6268b9ccfebeb4a8db6a
Patrick Carlson <patcarls@cisco.com>
2018-06-17 22:24:12 -0400

    Changed License

--- run.py
+++ run.py
@@ -1,16 +1,24 @@
+import logging
+
 from utilities import menu_strings
 
+def main():
+    print(menu_strings.welcome_string)
+    exit = False
+    while(exit is False):
+        print(menu_strings.main_menu)
+        option = input("> ")
+        print("----------------------------------------------")
+        if option.isdigit():
+            option = int(option)
+            print(f"You selected {option}")
+            if option == 0:
+                exit = True
+        else:
+            print(menu_strings.invalid_input)
 
-print(menu_strings.welcome_string)
-exit = False
-while(exit is False):
-    print(menu_strings.main_menu)
-    option = input("> ")
-    print("----------------------------------------------")
-    if option.isdigit():
-        option = int(option)
-        print(f"You selected {option}")
-        if option == 0:
-            exit = True
-    else:
-        print(menu_strings.invalid_input)
+def build_logger():
+
+
+if __name__ == '__main__':
+    main()
